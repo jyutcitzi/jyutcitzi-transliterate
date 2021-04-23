639ecd8f6dc265d93cf54538bab1728bc8717f6a
jyutcitzi <jyutcitzi_now@protonmail.com>
2021-04-23 22:37:15 +0100

    initial commit

---
+++ transliterate.py
@@ -0,0 +1,482 @@
+import ToJyutping
+import pycantonese as pc
+import sqlite3
+import wordsegment
+from words_to_jyutping import word_to_jyutping
+from mouth_radicals import mouth_list
+
+## TODO: add option to use 改革漢字, e.g. for 哋
+## TODO: add option for further cantonification of English pronunciations
+
+puncs = '，！？：；（）［］【】。「」、‧《》〈〉'
+# define all of the components
+onsets = {'b': '比', 'p': '并', 'm': '文', 'f': '夫', 'd': '大', 't': '天',
+          'n': '乃', 'l': '力', 'z': '止', 'c': '此', 's': '厶', 'j': '央',
+          'g': '丩', 'k': '臼', 'h': '亾', 'ng': '爻', 'gw': '古', 'kw': '夸',
+          'w': '禾', None: '', '': ''}
+finals = {'aa':'乍', 'aai':'介', 'aau':'丂', 'aam':'彡', 'aan':'万',
+          'aang':'生', 'aap':'甲', 'aat':'压', 'aak':'百', 'ai':'兮', 'au':'久',
+          'am':'今', 'an':'云', 'ang':'亙', 'ap':'十', 'at':'乜', 'ak':'仄',
+          'e':'旡', 'ei':'丌', 'eu':'了', 'em':'壬', 'en':'円', 'eng':'正',
+          'ep':'夾', 'et':'叐', 'ek':'尺', 'i':'子', 'iu':'么', 'im':'欠',
+          'in':'千', 'ing':'丁', 'ip':'頁', 'it':'必', 'ik':'夕', 'o':'个',
+          'oi':'丐', 'ou':'冇', 'on':'干', 'ong':'王', 'ot':'匃', 'ok':'乇',
+          'u':'乎', 'ui':'会', 'un':'本', 'ung':'工', 'ut':'末', 'uk':'玉',
+          'oe':'居', 'oeng':'丈', 'oek':'勺', 'eoi':'句', 'eon':'卂', 'eot':'𥘅',
+          'yu':'仒', 'yun':'元', 'yut':'乙', 'ng': '爻', None: '', '': ''}
+tones = {'2': '´', '3': '`', '5': '˝', '6': 'ﾞ', None: '', '':''}
+syllabics = {'ng': '五`', 'm': '五.'}
+
+def get_hex_dict(file_loc, ng_tilde=False):
+    # Note: "tilde" is supposed to mean tick here
+    hex_dict = {'々': '々'}
+    with open(file_loc) as file:
+        for line in file.read().splitlines():
+            hex_str, chars = line.split(" ")
+            chars = chars.replace("oe","居")
+            hex_dict[chars] = chr(int(hex_str, 16))
+    if ng_tilde:
+        hex_dict['五`'] = hex_dict['ng`']
+        hex_dict['五.'] = hex_dict['m`']
+    else:
+        hex_dict['五`'] = hex_dict['ng_m']
+        hex_dict['五.'] = hex_dict['ng_m']
+
+    return hex_dict
+hex_dict = get_hex_dict("mapping.txt", ng_tilde=True)
+
+def replace_r(syllable):
+  # only replace r instances which are not in the first position
+  return syllable[0] + syllable[1:].replace('r','w')
+def factorize(syllable, replace_r=True):
+  # literally the only exception which doesn't work, hardcode here
+  if syllable == 'hng6': # 哼
+    return 'h', 'ng', '6', ''
+  # the number at the end of the string is the tone
+  if syllable[-1].isdigit():
+    tone = syllable[-1]
+    syllable = syllable[:-1]
+  else:
+    tone = None
+  # everything before the first vowel is the onset
+  onset = ""
+  i = 0
+  while i < len(syllable) and (syllable[i] not in "aeiouy"):
+    onset += syllable[i]
+    i += 1
+  if replace_r and len(onset) > 1 and onset[-1] == 'r':
+    onset = onset[:-1] + 'w'
+
+  remainder = syllable[i:]
+  index = len(remainder)
+  while index >= 0 and remainder[0:index] not in finals:
+    index -= 1
+    if remainder[0:index] in finals:
+      break
+
+  final = remainder[0:index]
+  suffix = remainder[index:]
+  is_syllabic = onset in {'m', 'ng'} and final == '' and suffix == ''
+  return onset, final, suffix, tone, is_syllabic
+
+def assemble(jping_tup, mode='font'):
+  assert mode in ['web','font']
+  onset, final, suffix, tone, is_syllabic = jping_tup
+  if is_syllabic:
+    thing = onset + "`" + tones[tone]
+    if mode == 'web':
+      return syllabics[onset] + tones[tone] + ("·" if tone in {"", None} else "")
+    elif mode == 'font':
+      return hex_dict[thing]
+  else:
+    has_zero = (onset == '' and final != '') or (onset != '' and final == '')
+    thing = (onsets[onset] if onset in onsets else "".join(onsets[c] for c in onset)) \
+          + finals[final] + ("`" if has_zero else "") + tones[tone]
+    suffix_thing = "".join(onsets[c] for c in suffix) + \
+      ("`" if len(suffix) == 1 else "")
+
+    if mode == 'web':
+      return thing + ("·" if tone in {"", None} else "") \
+        + (suffix_thing + "·" if suffix != "" else "")
+    elif mode == 'font':
+      return hex_dict[thing] + (hex_dict[suffix_thing] if suffix != "" else "")
+
+def transliterate(input, mode='font', orthography='honzi_jcz', use_repeat_char=True,
+                  initial_r_block="r", v_block="v", tone_config='horizontal', use_schwa_char=False):
+  assert mode in {'font', 'web'}
+  assert orthography in {'jcz_only', 'honzi_jcz'}
+  assert initial_r_block in {'r', 'wl', 'w'}
+  assert v_block in {'v','f'}
+  # use_schwa_char
+  assert tone_config in {'horizontal','vertical'}
+
+  # address component customizations
+  # initial_r_block
+  if initial_r_block == 'r':
+    onsets['r'] = 'ㄖ'
+  elif initial_r_block == 'wl':
+    onsets['r'] = '禾力'
+  elif initial_r_block == 'w':
+    onsets['r'] = '禾'
+  # v_block
+  onsets['v'] = '圭' if v_block == 'v' else '夫'
+  # use_schwa_char
+  finals['a'] = '亇' if use_schwa_char else '乍'
+  # tone_config
+  tones['1'] = '¯' if tone_config == 'horizontal' else '\''
+  tones['4'] = '⁼' if tone_config == 'horizontal' else '\"'
+
+  # load CMU dictionary for using pronunciations
+  con_cmu = sqlite3.connect('cmudict.db')
+  cur_cmu = con_cmu.cursor()
+
+  # load word segmentator
+  wordsegment.load()
+
+  outs, unparsed, word = [], set(), ""
+  # ToJyutping has better pronunciation values compared to PyCantonese
+  tj_parse = ToJyutping.get_jyutping_list(input)
+
+  # parse words properly
+  new_tj_parse, word, ptr = [], "", 0
+  # A-Z and a-z only
+  is_alphabetic = lambda x: x.isascii() and x.isalpha()
+  is_alphanum = lambda x: x.isascii() and x.isalnum()
+  while ptr < len(tj_parse):
+    glyph, jping = tj_parse[ptr]
+    if not is_alphabetic(glyph): # note sinoglyphs are alphabetic in unicode
+      if glyph != " ":
+        new_tj_parse.append(tj_parse[ptr])
+      ptr += 1
+    else:
+      word = ""
+      while is_alphabetic(glyph) and jping is None and ptr < len(tj_parse):
+        glyph, jping = tj_parse[ptr]
+        if is_alphabetic(glyph) and jping is None:
+          word += glyph
+          ptr += 1
+      if word != " ":
+        new_tj_parse.append((word, None))
+
+  # perform the conversion to Honzi-Jyutping mix
+  for i, pair in enumerate(new_tj_parse):
+    glyphs, jpings = pair
+    if jpings is not None:
+      jping_arr = pc.parse_jyutping(jpings.replace(" ",""))
+      for j, glyph in enumerate(glyphs):
+        if orthography == 'jcz_only':
+          outs.extend(jpings.split(" "))
+        elif orthography == 'honzi_jcz':
+          outs.append(str(jping_arr[j]) if glyph in mouth_list else glyph)
+    elif glyphs in puncs or glyphs.isdigit():
+      outs.append(glyphs)
+    else:
+      # attempt word segmentation to guesstimate pronuncation...
+      words = wordsegment.segment(glyphs)
+      # then attempt to convert the words into jyutping
+      temp = []
+      failed = False
+      for word in words:
+        parsed = word_to_jyutping(word, cur_cmu=cur_cmu)
+        if parsed is not None:
+          temp.extend(parsed)
+        else:
+          print("Warning: "+word+" from "+glyphs+" is not converted into Jyutping")
+          failed = True
+          break
+      if failed:
+        # fallback is to append original word and to not convert it into Jyutping
+        # e.g. because it is a company name
+        outs.append(glyphs)
+        # keep track to make sure no attempts are made in converting it into Jyutping
+        unparsed.add(glyphs)
+      else:
+        # jyutping successfully obtained
+        outs.extend(temp)
+
+
+
+  # convert Honzi-Jyutping to target orthography (JCZ-only or Honzi-JCZ)
+  outs = [assemble(factorize(out), mode=mode) if is_alphanum(out) and len(out) > 1 and out not in unparsed else out for out in outs]
+
+  # refine using repeat characters
+  if use_repeat_char:
+    if len(outs) > 1:
+      new_outs = [outs[0]]
+      i = 1
+      while i < len(outs):
+        if outs[i] == outs[i-1] and not (is_alphanum(outs[i])):
+          new_outs.append("々")
+        else:
+          new_outs.append(outs[i])
+        i += 1
+      outs = new_outs
+
+  # close CMU dictionary
+  con_cmu.close()
+
+  return "".join(outs)
+
+
+
+
+
+
+
+import ToJyutping
+import pycantonese as pc
+
+## TODO: add option to use 改革漢字, e.g. for 哋
+## TODO: add option for further cantonification of English pronunciations
+
+puncs = '，！？：；（）［］【】。「」、‧《》〈〉'
+# define all of the components
+onsets = {'b': '比', 'p': '并', 'm': '文', 'f': '夫', 'd': '大', 't': '天',
+          'n': '乃', 'l': '力', 'z': '止', 'c': '此', 's': '厶', 'j': '央',
+          'g': '丩', 'k': '臼', 'h': '亾', 'ng': '爻', 'gw': '古', 'kw': '夸',
+          'w': '禾', None: '', '': ''}
+finals = {'aa':'乍', 'aai':'介', 'aau':'丂', 'aam':'彡', 'aan':'万',
+          'aang':'生', 'aap':'甲', 'aat':'压', 'aak':'百', 'ai':'兮', 'au':'久',
+          'am':'今', 'an':'云', 'ang':'亙', 'ap':'十', 'at':'乜', 'ak':'仄',
+          'e':'旡', 'ei':'丌', 'eu':'了', 'em':'壬', 'en':'円', 'eng':'正',
+          'ep':'夾', 'et':'叐', 'ek':'尺', 'i':'子', 'iu':'么', 'im':'欠',
+          'in':'千', 'ing':'丁', 'ip':'頁', 'it':'必', 'ik':'夕', 'o':'个',
+          'oi':'丐', 'ou':'冇', 'on':'干', 'ong':'王', 'ot':'匃', 'ok':'乇',
+          'u':'乎', 'ui':'会', 'un':'本', 'ung':'工', 'ut':'末', 'uk':'玉',
+          'oe':'居', 'oeng':'丈', 'oek':'勺', 'eoi':'句', 'eon':'卂', 'eot':'𥘅',
+          'yu':'仒', 'yun':'元', 'yut':'乙', 'ng': '爻', None: '', '': ''}
+tones = {'2': '´', '3': '`', '5': '˝', '6': 'ﾞ', None: '', '':''}
+syllabics = {'ng': '五`', 'm': '五.'}
+
+def get_hex_dict(file_loc, ng_tilde=False):
+    # Note: "tilde" is supposed to mean tick here
+    hex_dict = {'々': '々'}
+    with open(file_loc) as file:
+        for line in file.read().splitlines():
+            hex_str, chars = line.split(" ")
+            chars = chars.replace("oe","居")
+            hex_dict[chars] = chr(int(hex_str, 16))
+    if ng_tilde:
+        hex_dict['五`'] = hex_dict['ng`']
+        hex_dict['五.'] = hex_dict['m`']
+    else:
+        hex_dict['五`'] = hex_dict['ng_m']
+        hex_dict['五.'] = hex_dict['ng_m']
+
+    return hex_dict
+hex_dict = get_hex_dict("mapping.txt", ng_tilde=True)
+
+def replace_r(syllable):
+  # only replace r instances which are not in the first position
+  return syllable[0] + syllable[1:].replace('r','w')
+def factorize(syllable, replace_r=True):
+  # literally the only exception which doesn't work, hardcode here
+  if syllable == 'hng6': # 哼
+    return 'h', 'ng', '6', ''
+  # the number at the end of the string is the tone
+  if syllable[-1].isdigit():
+    tone = syllable[-1]
+    syllable = syllable[:-1]
+  else:
+    tone = None
+  # everything before the first vowel is the onset
+  onset = ""
+  i = 0
+  while i < len(syllable) and (syllable[i] not in "aeiouy"):
+    onset += syllable[i]
+    i += 1
+  if replace_r and len(onset) > 1 and onset[-1] == 'r':
+    onset = onset[:-1] + 'w'
+
+  remainder = syllable[i:]
+  index = len(remainder)
+  while index >= 0 and remainder[0:index] not in finals:
+    index -= 1
+    if remainder[0:index] in finals:
+      break
+
+  final = remainder[0:index]
+  suffix = remainder[index:]
+  is_syllabic = onset in {'m', 'ng'} and final == '' and suffix == ''
+  return onset, final, suffix, tone, is_syllabic
+
+def assemble(jping_tup, mode='font'):
+  assert mode in ['web','font']
+  onset, final, suffix, tone, is_syllabic = jping_tup
+  if is_syllabic:
+    thing = onset + "`" + tones[tone]
+    if mode == 'web':
+      return syllabics[onset] + tones[tone] + ("·" if tone in {"", None} else "")
+    elif mode == 'font':
+      return hex_dict[thing]
+  else:
+    has_zero = (onset == '' and final != '') or (onset != '' and final == '')
+    thing = (onsets[onset] if onset in onsets else "".join(onsets[c] for c in onset)) \
+          + finals[final] + ("`" if has_zero else "") + tones[tone]
+    suffix_thing = "".join(onsets[c] for c in suffix) + \
+      ("`" if len(suffix) == 1 else "")
+
+    if mode == 'web':
+      return thing + ("·" if tone in {"", None} else "") \
+        + (suffix_thing + "·" if suffix != "" else "")
+    elif mode == 'font':
+      return hex_dict[thing] + (hex_dict[suffix_thing] if suffix != "" else "")
+
+def transliterate(input, mode='font', orthography='honzi_jcz', use_repeat_char=True,
+                  initial_r_block="r", v_block="v", tone_config='horizontal', use_schwa_char=False):
+  assert mode in {'font', 'web'}
+  assert orthography in {'jcz_only', 'honzi_jcz'}
+  assert initial_r_block in {'r', 'wl', 'w'}
+  assert v_block in {'v','f'}
+  # use_schwa_char
+  assert tone_config in {'horizontal','vertical'}
+
+  # address component customizations
+  # initial_r_block
+  if initial_r_block == 'r':
+    onsets['r'] = 'ㄖ'
+  elif initial_r_block == 'wl':
+    onsets['r'] = '禾力'
+  elif initial_r_block == 'w':
+    onsets['r'] = '禾'
+  # v_block
+  onsets['v'] = '圭' if v_block == 'v' else '夫'
+  # use_schwa_char
+  finals['a'] = '亇' if use_schwa_char else '乍'
+  # tone_config
+  tones['1'] = '¯' if tone_config == 'horizontal' else '\''
+  tones['4'] = '⁼' if tone_config == 'horizontal' else '\"'
+
+  # load CMU dictionary for using pronunciations
+  con_cmu = sqlite3.connect('cmudict.db')
+  cur_cmu = con_cmu.cursor()
+
+  # load word segmentator
+  wordsegment.load()
+
+  outs, unparsed, word = [], set(), ""
+  # ToJyutping has better pronunciation values compared to PyCantonese
+  tj_parse = ToJyutping.get_jyutping_list(input)
+
+  # parse words properly
+  new_tj_parse, word, ptr = [], "", 0
+  # A-Z and a-z only
+  is_alphabetic = lambda x: x.isascii() and x.isalpha()
+  is_alphanum = lambda x: x.isascii() and x.isalnum()
+  while ptr < len(tj_parse):
+    glyph, jping = tj_parse[ptr]
+    if not is_alphabetic(glyph): # note sinoglyphs are alphabetic in unicode
+      if glyph != " ":
+        new_tj_parse.append(tj_parse[ptr])
+      ptr += 1
+    else:
+      word = ""
+      while is_alphabetic(glyph) and jping is None and ptr < len(tj_parse):
+        glyph, jping = tj_parse[ptr]
+        if is_alphabetic(glyph) and jping is None:
+          word += glyph
+          ptr += 1
+      if word != " ":
+        new_tj_parse.append((word, None))
+
+  # perform the conversion to Honzi-Jyutping mix
+  for i, pair in enumerate(new_tj_parse):
+    glyphs, jpings = pair
+    if jpings is not None:
+      jping_arr = pc.parse_jyutping(jpings.replace(" ",""))
+      for j, glyph in enumerate(glyphs):
+        if orthography == 'jcz_only':
+          outs.extend(jpings.split(" "))
+        elif orthography == 'honzi_jcz':
+          outs.append(str(jping_arr[j]) if glyph in mouth_list else glyph)
+    elif glyphs in puncs or glyphs.isdigit():
+      outs.append(glyphs)
+    else:
+      # attempt word segmentation to guesstimate pronuncation...
+      words = wordsegment.segment(glyphs)
+      # then attempt to convert the words into jyutping
+      temp = []
+      failed = False
+      for word in words:
+        parsed = word_to_jyutping(word, cur_cmu=cur_cmu)
+        if parsed is not None:
+          temp.extend(parsed)
+        else:
+          print("Warning: "+word+" from "+glyphs+" is not converted into Jyutping")
+          failed = True
+          break
+      if failed:
+        # fallback is to append original word and to not convert it into Jyutping
+        # e.g. because it is a company name
+        outs.append(glyphs)
+        # keep track to make sure no attempts are made in converting it into Jyutping
+        unparsed.add(glyphs)
+      else:
+        # jyutping successfully obtained
+        outs.extend(temp)
+
+
+
+  # convert Honzi-Jyutping to target orthography (JCZ-only or Honzi-JCZ)
+  outs = [assemble(factorize(out), mode=mode) if is_alphanum(out) and len(out) > 1 and out not in unparsed else out for out in outs]
+
+  # refine using repeat characters
+  if use_repeat_char:
+    if len(outs) > 1:
+      new_outs = [outs[0]]
+      i = 1
+      while i < len(outs):
+        if outs[i] == outs[i-1] and not (is_alphanum(outs[i])):
+          new_outs.append("々")
+        else:
+          new_outs.append(outs[i])
+        i += 1
+      outs = new_outs
+
+  # close CMU dictionary
+  con_cmu.close()
+
+  return "".join(outs)
+
+def file_transliterator(file, mode='font', orthography='honzi_jcz', use_repeat_char=True,
+                  initial_r_block="r", v_block="v", tone_config='horizontal', use_schwa_char=False):
+    with open(args.file, 'r') as f:
+        return transliterate(f.read(), mode, orthography, use_repeat_char,
+                          initial_r_block, v_block, tone_config, use_schwa_char)
+
+    return "Error: couldn't read " + file
+
+## code for running command
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("file", help="read input text from FILE", metavar="FILE")
+    parser.add_argument("-m", "--mode", dest="mode",
+                        help="use font or web-style characters", metavar="MODE", default='font')
+    parser.add_argument("-s", "--style", dest="orthography",
+                        help="use jcz-only (jcz_only) or sinoglyph-jcz (honzi_jcz) writing style", metavar="STYLE", default='honzi_jcz')
+    parser.add_argument("-r","--r_block", dest="initial_r_block",
+                        help="whether to use r, wl or w for representing the onset 'r'",
+                        metavar="R", default='r')
+    parser.add_argument("-v","--v_block", dest="v_block",
+                        help="whether to use v or f for representing the onset 'v'",
+                        metavar="V", default='v')
+    parser.add_argument("-t","--tone_config", dest="tone_config",
+                        help="whether to use vertical or horizontal ticks for tones 1 and 4",
+                        metavar="DIRECTION", default='horizontal')
+    parser.add_argument("--use_repeat_char", dest="use_repeat_char",
+                        help="whether to use the repeat glyph 々", metavar='BOOL', type=bool, default=True)
+    parser.add_argument("--use_schwa_char", dest="use_schwa_char",
+                        help="whether to use 亇 for the final 'a' instead of 乍", metavar='BOOL', type=bool, default=False)
+
+
+    args = parser.parse_args()
+
+    print(file_transliterator(**vars(args)))
+
+    # print(args)
+
+
+    #input, mode='font', orthography='honzi_jcz', use_repeat_char=True,
+                      # initial_r_block="r", v_block="v", tone_config='horizontal', use_schwa_char=False
---
+++ words_to_jyutping.py
@@ -0,0 +1,237 @@
+import sqlite3, re, json, string
+# just put in lowercase for consonants
+consonants = {'B': 'b',
+              'CH': 'ch',
+              'D': 'd',
+              'DH': 'd',
+              'F': 'f',
+              'G': 'g',
+              'HH': 'h',
+              'JH': 'zj',
+              'K': 'k',
+              'L': 'l',
+              'M': 'm',
+              'N': 'n',
+              'NG': 'ng',
+              'P': 'p',
+              'R': 'r',
+              'S': 's',
+              'SH': 'sh',
+              'T': 't',
+              'TH': 'f',
+              'V': 'f',
+              'W': 'w',
+              'Y': 'j',
+              'Z': 's',
+              'ZH': 'sh'
+              }
+end_conses = {'B': 'p',
+              'CH': 'cj',
+              'D': 't',
+              'F': 'f',
+              'G': 'k',
+              # 'JH': 'zj',
+              'K': 'k',
+              'L': '',
+              'M': 'm',
+              'N': 'n',
+              'NG': 'ng',
+              'P': 'p',
+              'S': 's',
+              'T': 't',
+              'TH': 'f',
+              'Z': 's'}
+# AA R => aa, but AE R => e aa
+# faster => faaas toe
+# beast => bist
+vowels = {'AA': 'aa',
+         'AE': 'e',
+         'AH': 'a',
+         'AO': 'o', # AO L => o small
+         'AW': 'aau',
+         'AY': 'aai', # exception: AY T => ait (e.g. night)
+         'EH': 'e',
+         'ER': 'oe', # aa if last mark
+         'EY': 'ei',
+         'IH': 'i',
+         'IY': 'i',
+         'OW': 'ou',
+         'OY': 'oi',
+         'UH': 'u',
+         'UW': 'u'}
+# append to vowel if next is m, n, ng, p, t, k, b, d or g,
+special_cases = {
+    'AE L': 'eu',
+    'AA R': 'aa',
+    'AW T': 'aut'
+}
+
+devoice = {'b': 'p', 'd': 't', 'g': 'k'}
+# every syllable is [C]V(C)
+
+def word_to_jyutping(s, cur_cmu=None, debug=False):
+    locally_open_cmu = False
+    if cur_cmu is None:
+        con_cmu = sqlite3.connect('cmudict.db')
+        cur_cmu = con_cmu.cursor()
+        locally_open_cmu = True
+    assert cur_cmu is not None
+    # attempts to convert a single english word to jcz using the cmud database
+    # input should only be ASCII, and should not have spaces
+    # if nothing is found, the input falls through
+    if ' ' in s:
+        return s                # falls through
+    try:
+        s.encode('ascii')
+    except:
+        return s                # falls through
+    # try to look in cmudict
+    cur_cmu.execute('SELECT pronunciation FROM dictionary WHERE word LIKE ?', (s,))
+    p = cur_cmu.fetchone()
+    if p is None:
+      return None
+    p = p[0]
+    # now start to match the pronunciation
+    # clean up the pronunciation of numbers (the emphasis marks)
+    p = re.sub('\d+', '', p)
+    split = p.split(' ')
+    if debug:
+      print(split)
+    i = 0
+    outs = []
+    syllable = ''
+    carry_w = False
+    carry_r = False
+    while i < len(split):
+      if carry_r:
+        syllable += "r"
+        carry_r = False
+      if carry_w:
+        syllable += "w"
+        carry_w = False
+      curr_consts = ""
+
+      while split[i] in consonants:
+        if i == 0 and split[0] == 'V':
+          curr_consts += 'v'
+        else:
+          curr_consts += consonants[split[i]]
+        i += 1
+        if i == len(split):
+          if curr_consts[0] == 'r':
+            if curr_consts[1:] in devoice:
+              outs.append('aa'+devoice[curr_consts[1:]])
+            else:
+              outs.append('aa'+curr_consts[1:])
+          elif len(curr_consts) == 1:
+            outs[-1] += curr_consts
+          else:
+            outs.append(curr_consts)
+          return outs
+      if curr_consts == 'th':
+        curr_consts = 'd'
+      syllable += curr_consts
+
+      # split[i] is a vowel
+      if split[i] == 'ER' and i == len(split) - 1:
+        vowel = 'aa'
+      else:
+        vowel = vowels[split[i]]
+      other_bit = ""
+      if i+1 < len(split) and split[i] == 'ER' and split[i+1] in vowels:
+        carry_r = True
+
+      if i > 0 and i+1 < len(split) and split[i] == 'AH' and split[i+1] == 'L':
+        # i==0: alacrity
+        if i > 1 and  split[i-1] == 'Y':
+          # calculator
+          syllable = syllable[:-1] + 'i'
+          vowel = 'u'
+          i += 1
+        elif i == len(split)-2:
+          # technical
+          vowel = 'ou'
+          i += 2
+        else:
+          vowel = 'a'
+          i += 1
+      elif split[i-1] == 'Y' and split[i] == 'UW':
+        syllable = syllable[:-1] + "i"
+        vowel = 'u'
+        i += 1
+      elif split[i] == 'AE' and split[i+1] == 'P' and split[i+2] in vowels:
+        vowel = 'ep'
+        i += 1
+      elif i+2 < len(split) and split[i] == 'AE' and split[i+1] in {'G','K'} and split[i+2] == 'L':
+        other_bit = 'k'
+        i += 2
+      elif split[i] == 'AW' and split[i+1] in vowels:
+        i += 1
+        carry_w = True
+      elif i+1 < len(split) and split[i] == 'AO' and split[i+1] == 'R':
+        vowel = 'o'
+        if i+2 < len(split) and split[i+2] in end_conses:
+          other_bit = end_conses[split[i+2]]
+          i += 1
+        i += 2
+      elif i+1 < len(split) and split[i]+' '+split[i+1] in special_cases:
+        vowel = special_cases[split[i]+' '+split[i+1]]
+        i += 2
+      else:
+        if i+2 < len(split) and split[i+1] in {'B','G','P','K'} \
+        and split[i+2] in {'W','Y','L','R'}:
+          other_bit = end_conses[split[i+1]]
+          if split[i+1] == 'B' and split[i+2] == 'W':
+            i += 2
+          else:
+            i += 1
+        elif i+1 < len(split) and split[i][-1] == 'H' and split[i+1] in end_conses:
+          other_bit = end_conses[split[i+1]]
+          if i+2 < len(split) and split[i+2] in vowels:
+            i += 1
+          elif i+2 < len(split) and split[i+2] == 'Y':
+            i += 1
+          else:
+            i += 2
+        elif i+1 < len(split) and split[i+1] in end_conses:
+          if split[i] == 'AY' and split[i+1] in {'P','T','K'}:
+            vowel = 'ai' # hike is haik not haaik
+            i += 1
+          elif i+2 < len(split) and split[i+2] in vowels:
+            # other_bit = end_conses[split[i+1]]
+            i += 1
+          elif i+2 < len(split) and split[i+2] in 'R': # {'W','Y','L','R'}
+            other_bit = end_conses[split[i+1]]
+            i += 1
+          else:
+            other_bit = end_conses[split[i+1]]
+            i += 2
+        else:
+          i += 1
+
+      #further adjustment
+      # now len(split)-2 is N/K and i=len(split)-1 is T
+      if (i == len(split)-1 or (i+1 < len(split) and split[i+1] in consonants)) and split[i-1] in {'N','K'} and split[i] in {'T','D'}:
+        # skip the T as well
+        i += 1
+      elif i+1 < len(split) and split[i] == 'P' and split[i+1] in consonants \
+            and split[i+1] not in {'W','Y','L','R'}:
+        # split[i+1] cannot be semivowel W/Y
+        # skip the P
+        i += 1
+      elif i < len(split) and split[i-1] == 'NG' and split[i] == 'K':
+        i += 1
+        # skip the K
+      if i + 1 < len(split) and vowel != "" and other_bit != "" and \
+          split[i] in {'S','Z'} and split[i+1] in consonants:
+        other_bit += 's'
+        i += 1
+
+      syllable += vowel
+      syllable += other_bit
+      outs.append(syllable)
+      syllable = ""
+
+      if locally_open_cmu:
+        con_cmu.close()
+    return outs
